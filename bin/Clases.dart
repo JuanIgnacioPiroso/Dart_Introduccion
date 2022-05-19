a6fcff68c331399191cffa2007287e5a8ade6f61
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-05-19 18:35:38 -0300

    Practice exercises

---
+++ bin/Clases.dart
@@ -0,0 +1,20 @@
+void main() {
+  Heroe wolverine = new Heroe(nombre: 'Logan',  poder: 'Regeneracion');
+
+  // wolverine.nombre = 'Logan';
+  // wolverine.poder = 'Regeneracion';
+
+  print(wolverine);
+}
+
+class Heroe {
+  String nombre;
+  String poder;
+
+  Heroe({required this.nombre, required this.poder});
+
+  @override
+  String toString() {
+    return 'Heroe: nombre: ${this.nombre}, poder: ${this.poder}';
+  }
+}
