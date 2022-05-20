18b18b3893db62f4509ceb695cf4ff54a563a5f3
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-05-19 21:02:01 -0300

    Practice exercises

---
+++ bin/Constructores.dart
@@ -0,0 +1,25 @@
+void main() {
+  final rawJson = {'nombre': 'Tony Stark', 'poder': 'Dinero'};
+
+  Heroe ironman = Heroe.fromJson(rawJson);
+
+  Heroe wolverine = new Heroe(nombre: 'Logan', poder: 'Regeneracion');
+
+  print(ironman);
+}
+
+class Heroe {
+  String nombre;
+  String poder;
+
+  Heroe({required this.nombre, required this.poder});
+
+  Heroe.fromJson(Map<String, String> json)
+      : this.nombre = json['nombre']!,
+        this.poder = json['poder'] ?? 'No tiene poder';
+
+  @override
+  String toString() {
+    return 'Heroe: nombre: ${this.nombre}, poder: ${this.poder}';
+  }
+}
