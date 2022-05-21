54a2e091a08cfcfca6cb882c4aed96a480bfd3e6
JuanIgnacioPiroso <jpiroso@frba.utn.edu.ar>
2022-05-21 00:46:49 -0300

    Practice exercises

---
+++ bin/Futures.dart
@@ -0,0 +1,15 @@
+void main() {
+  print('Antes de la peticion');
+
+  httpGet('https://api.nasa.com/aliens').then((data) {
+    print(data);
+  });
+
+  print('Fin del programa');
+}
+
+Future<String> httpGet(String url) {
+  return Future.delayed(Duration(seconds: 3), () {
+    return 'Hola mundo - 3 segundos';
+  });
+}
